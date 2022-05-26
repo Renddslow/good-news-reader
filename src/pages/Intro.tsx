be57d0af12d1ac7c9091efdb35d26a523adddb08
Matt McElwee <matt.mcelwee@jointhebibleproject.com>
2022-05-26 15:52:38 -0400

    feat: added the remainder of the intro content

--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -102,6 +102,13 @@ const Intro = () => {
                   '<p>The book of Revelation has four primary movements which are surrounded by a short prologue and a short epilogue. We can identify them in the text by the use of the repeated phrase "in the Spirit" (Revelation 1:10, 4:2, 17:3, 21:10).<a href="#ref1" class="has-sup"><sup id="ref1_fn">1</sup></a> These give us helpful markers to see that John is hinting at how he wants us to think about the structure of this book.</p><p>We\'ll follow this structure, reading a movement over the course of one to two weeks and seeing how each movement contributes to the unified vision of both the book of Revelation and that of the whole Bible.</p>',
               }}
             />
+            <Markdown
+              data={{
+                title: 'Links and Progress',
+                content: `<p>As you go you'll be rewarded with <strong>progress</strong> markers and <strong>links</strong>.</p><p>Progress markers indicate that you've read or otherwise engaged with one of the pages in the reader. Some pages have videos, some have a short explainer essay, and most have the words of Scripture (it is a Bible reader, after all). There are 22 pages in total, and you'll be able to track your progress in the header at the top of each page.</p><p>Links are a new feature to Flatland's Bible readers. We mentioned above that the book of Revelation is <em>filled</em> with references, allusions, themes, and literary patterns that show up throughout the rest of Scripture. Keep an eye out for these links as you read. If you click them, we'll give you a short explainer and the Scripture (or Scriptures) that John was referencing. By clicking them, you'll also add them to your overall link count.</p><p>Remember, the goal isn't just to collect progress markers or links, but to actively read the Bible as a unified story that leads to Jesus in an unhurried and thoughtful way.</p><p>We hope that this will help you on the journey.</p>`,
+              }}
+              titleSize="h2"
+            />
             <Footnotes>
               <p id="ref1">
                 <a href="#ref1_fn">[1]</a>: This was first identified by Richard Bauckham in his
