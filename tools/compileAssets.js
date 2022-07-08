3c92310454dd226220d614267f55ef2d3fe3de34
Matt McElwee <matt.mcelwee@jointhebibleproject.com>
2022-07-08 11:05:03 -0500

    feat: fixed the asset compiler for the new asset process

--- tools/compileAssets.js
+++ tools/compileAssets.js
@@ -14,43 +14,11 @@ const enhancedSnarkdown = (markdown) =>
     )
     .join('\n');
 
-const outputJson = (o, movement, item) => {
-  const filepath = path.join(process.cwd(), 'public/assets', `movement.${movement}.${item}.json`);
+const outputJson = (o, page) => {
+  const filepath = path.join(process.cwd(), 'public/assets', `${page}.json`);
   return fs
     .writeFile(filepath, JSON.stringify(o))
     .then(() => console.log(`${kleur.gray('Created JSON asset at')} ${filepath}`));
-};
-
-const chapterRefToInt = (ref) => {
-  const [, ch] = /([\d]+)$/.exec(ref);
-  return parseInt(ch, 10);
-};
-
-const findVerse =
-  (ref, type = 'verse') =>
-  (block) =>
-    block.children.some(
-      (c) =>
-        c.type === type &&
-        c.reference.chapter === chapterRefToInt(ref.chapter) &&
-        c.reference.verse === ref.verse,
-    );
-
-const trim = (asset, start, end) => {
-  const blockStartIdx = asset.content.findIndex(findVerse(start));
-
-  const blockEndIdx = asset.content.findIndex(findVerse(end, 'verse_end'));
-  const verseEndIdx = asset.content[blockEndIdx].children.findIndex(
-    (c) => c.type === 'verse_end' && c.reference.verse === end.verse,
-  );
-
-  asset.content[blockEndIdx].children = asset.content[blockEndIdx].children.slice(
-    0,
-    verseEndIdx + 1,
-  );
-
-  asset.content = asset.content.slice(blockStartIdx, blockEndIdx + 1);
-  return asset;
 };
 
 const getStringFromFile = async (p) => (await fs.readFile(path.join(process.cwd(), p))).toString();
@@ -58,44 +26,18 @@ const getStringFromFile = async (p) => (await fs.readFile(path.join(process.cwd(
 const compileAssets = async () => {
   await makeDir(path.join(process.cwd(), 'public/assets'));
 
-  const assets = JSON.parse(await getStringFromFile('asset-plan.json'));
+  const assets = JSON.parse(await getStringFromFile('assets.json'));
 
   await Promise.all(
-    assets.map(async (asset) => {
-      if (asset.type === 'video') {
-        asset.src = asset.src.replace('watch?v=', 'embed/');
-        if (asset.descriptionSrc) {
-          asset.description = enhancedSnarkdown(await getStringFromFile(asset.descriptionSrc));
-        }
+    assets.pages.map(async (page) => {
+      const chapters = await Promise.all(
+        page.chapters.map(async (chapter) => {
+          chapter.content = JSON.parse(await getStringFromFile(`data/final/${chapter.id}.json`));
+          return chapter;
+        }),
+      );
 
-        return outputJson(asset, asset.movement, asset.item);
-      }
-
-      if (asset.type === 'markdown') {
-        asset.content = enhancedSnarkdown(await getStringFromFile(asset.src)).replace(
-          '---',
-          '<hr />',
-        );
-        return outputJson(asset, asset.movement, asset.item);
-      }
-
-      if (asset.type === 'scripture') {
-        const [, startCh] = /(\d+)$/.exec(asset.start.chapter);
-        const [, endCh] = /(\d+)$/.exec(asset.end.chapter);
-        const chapters = Array(parseInt(endCh) - parseInt(startCh) + 1)
-          .fill()
-          .map((_, i) => parseInt(startCh) + i);
-
-        asset.content = (
-          await Promise.all(
-            chapters.map(async (ch) =>
-              JSON.parse(await getStringFromFile(`data/final/rev${ch}.json`)),
-            ),
-          )
-        ).reduce((acc, ch) => [...acc, ...ch], []);
-
-        await outputJson(trim(asset, asset.start, asset.end), asset.movement, asset.item);
-      }
+      await outputJson({ ...page, chapters }, page.page);
     }),
   );
 };
