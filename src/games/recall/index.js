fdfa5ff97ff74972bc0f7ed69f360fa026e4fcc9
Matt McElwee <matt.mcelwee@jointhebibleproject.com>
2022-07-21 10:23:54 -0500

    feat: added Matthew reading

---
+++ src/games/recall/index.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+const Recall = () => {
+  const [usedHint, setUsedHint] = useState(false);
+  const [used5050, setUsed5050] = useState(false);
+  const [skipsUsed, setSkipsUsed] = useState(0);
+  const [score, setScore] = useState(0);
+};
--- src/pages/Items/words.ts
+++ src/pages/Items/words.ts
@@ -3,46 +3,67 @@ const words = {
     lang: 'Hebrew',
     pos: 'noun',
     definition: 'life/living thing',
+    hint: '',
   },
   chayyim: {
     lang: 'Hebrew',
     pos: 'noun',
     definition: 'life',
+    hint: '',
   },
   zoe: {
     lang: 'Greek',
     pos: 'noun',
     definition: 'life',
+    hint: '',
   },
   zao: {
     lang: 'Greek',
     pos: 'verb',
     definition: 'to live',
+    hint: '',
   },
   zoopoieo: {
     lang: 'Greek',
     pos: 'verb',
     definition: 'to quicken/give life',
+    hint: '',
   },
   ahavah: {
     lang: 'Hebrew',
     pos: 'noun',
     definition: 'love',
+    hint: '',
   },
   ahav: {
     lang: 'Hebrew',
     pos: 'verb',
     definition: 'to love',
+    hint: '',
   },
   agapao: {
     lang: 'Greek',
     pos: 'verb',
     definition: 'to love',
+    hint: '',
   },
   agape: {
     lang: 'Greek',
     pos: 'noun',
     definition: 'love',
+    hint: '',
+  },
+  shem: {
+    lang: 'Hebrew',
+    pos: 'noun',
+    definition: 'name',
+    hint: '',
+  },
+  onoma: {
+    lang: 'Greek',
+    pos: 'noun',
+    definition: 'name',
+    hint: '',
   },
 };
 
